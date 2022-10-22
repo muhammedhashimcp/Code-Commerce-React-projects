0d979ba364e1c157eca4ef2d87c4722d8137f8fe
MUHAMMEDHASHIMCP <cpmohdhashim@gmail.com>
2022-10-22 10:41:15 +0530

    responsive navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import './App.css';
-import About from './components/About';
-import AllInOne from './components/AllInOne';
-import Footer from './components/Footer';
-import Hero from './components/Hero';
+import React from 'react';
 import Navbar from './components/Navbar';
-import Pricing from './components/Pricing';
-import Support from './components/Support';
+import Hero from './components/Hero';
 
 function App() {
-  return (
+	return (
 		<>
 			<Navbar />
 			<Hero />
-			<About />
-			<Support />
-			<AllInOne />
-			<Pricing />
-			<Footer />
 		</>
-  );
+	);
 }
 
 export default App;
